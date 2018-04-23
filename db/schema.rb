4307fb8a55c4fafef4b49ba394e6bb64c09a4bb7
Jaykumarp1 <jaykumarp1559@gmail.com>
2018-04-24 01:02:30 +0530

    modified the new.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_20_102050) do
+ActiveRecord::Schema.define(version: 2018_04_23_192002) do
 
   create_table "depts", force: :cascade do |t|
     t.string "name"
